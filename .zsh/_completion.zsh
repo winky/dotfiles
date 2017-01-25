05f7161bfb2788d0e8244da0ac391f3853c906b5
winky <y.kohada@gmail.com>
2017-01-25 10:06:31 +0900

    [Modify]Change color settings of ls command & completion

--- .zsh/_completion.zsh
+++ .zsh/_completion.zsh
@@ -1,8 +1,12 @@
+if [ -e $DOTFILES/.zsh/completion/zsh-completions ]; then
+  fpath=($DOTFILES/.zsh/completion/zsh-completions/src $fpath)
+fi
+
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z} r:|[-_.]=**'
 zstyle ':completion:*' verbose yes
 zstyle ':completion:*' format '%B%d%b'
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ${(s.:.)LSCOLORS}
 zstyle ':completion:*' use-cache true
 zstyle ':completion:*:default' menu select=2
 zstyle ':completion:*:options' description 'yes'
--- .zsh/_env.zsh
+++ .zsh/_env.zsh
@@ -15,7 +15,7 @@ export HISTSIZE=10000
 export SAVEHIST=1000000
 
 # ls command colors
-export LSCOLORS=exfxcxdxbxegedabagacad
-export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+export LSCOLORS=gxfxcxdxbxegedabagacad
+export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
 source $HOME/.iterm2_shell_integration.zsh
