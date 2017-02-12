4eba1d657739a97319056f0d1827e282d6ab09b8
winky <y.kohada@gmail.com>
2017-02-13 01:15:00 +0900

    [Add]PATH to env.zsh

--- .zsh/_env.zsh
+++ .zsh/_env.zsh
@@ -18,4 +18,6 @@ export SAVEHIST=1000000
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
+export PATH="/usr/local/bin:/usr/texbin:$PATH"
+
 source $HOME/.iterm2_shell_integration.zsh
