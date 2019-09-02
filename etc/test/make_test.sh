7783f94e9f24d6e14a912c4f2f6ac90e84ef0fcf
winky <y.kohada@gmail.com>
2019-09-03 02:13:44 +0900

    [Add] symlink test

--- etc/test/make_test.sh
+++ etc/test/make_test.sh
@@ -5,14 +5,34 @@ trap 'echo Error: $0; exit 1' ERR INT
 ERR=0
 
 deploy() {
-  cd "$DOTPATH"
+  cd $DOTPATH
   make deploy >/dev/null
   if [ $? -eq 0 ]; then
-    echo "Success dotfiles deploy"
+    echo "Success: Deploy dotfiles"
   else
-    echo "Fail dotfiles deploy"
+    echo "Fail: Deploy dotfiles"
     echo "$0: $LINENO: $FUNCNAME"
   fi
 }
 
+link() {
+  cd $DOTPATH
+  for i in $(make -s list | sed "s/[*@/]$//g"); do
+    if [ ! -L $HOME/$i ]; then
+      echo "Fail: Not found $i"
+      ERR=1
+    elif [ "$(readlink $HOME/$i)" != $DOTPATH/$i ]; then
+      echo "Fail: get link of $i"
+      ERR=1
+    fi
+  done
+
+  if [ "$ERR" = 0 ]; then
+    echo "Success: Check the file link"
+  else
+    echo "Fail: Check the file link"
+  fi
+}
+
 deploy
+link
