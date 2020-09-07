61c63682e93d8b2c6dafb90212abb76b71361d52
winky <y.kohada@gmail.com>
2020-09-07 19:25:50 +0900

    remove: hub command

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -11,11 +11,6 @@ zinit ice lucid wait'!0' pick'init.sh' \
   atload'ENHANCD_FILTER=peco:fzf:fzy; export ENHANCD_FILTER;'
 zinit light 'b4b4r07/enhancd'
 
-zinit ice lucid wait'!0' proto'ssh' from'gh-r' as'program' mv'hub* -> hub' \
-  atclone'prefix=$ZPFX ./hub/install; ln -sf ./hub/etc/hub.zsh_completion _hub;' \
-  atpull'%atclone' pick'$ZPFX/bin/hub*'
-zinit light 'github/hub'
-
 zinit ice lucid wait'!0' proto'ssh' from'gh-r' as'program'
 zinit light 'junegunn/fzf-bin'
 
