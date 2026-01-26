9468c93864f7e723ace63ed817cac497cf9bdab8
winky <y.kohada@gmail.com>
2026-01-26 23:46:15 +0900

    refactor: optimize .zsh/_env.zsh configuration
    - Fix typo: Langage -> Language
    - Fix HISTSIZE and SAVEHIST mismatch (10000 -> 100000)
    - Change HISTFILE from ~ to $HOME for clarity
    - Optimize PATH configuration with conditional checks
    - Remove obsolete paths (python2.7, texbin)
    - Support both Intel and Apple Silicon Macs

--- .zsh/_env.zsh
+++ .zsh/_env.zsh
@@ -1,22 +1,24 @@
 # Environmental variables
-# Langage setting
+# Language setting
 export LANG=ja_JP.UTF-8
 export LESSCHARSET=utf-8
-# Langage setting for ssh
+# Language setting for ssh
 export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 # History
 # History file
-export HISTFILE=~/.zsh_history
+export HISTFILE=$HOME/.zsh_history
 # History size in memory
-export HISTSIZE=10000
-# The number of histsize
-export SAVEHIST=1000000
+export HISTSIZE=100000
+# The number of history entries to save
+export SAVEHIST=100000
 
 # ls command colors
 export LSCOLORS=gxfxcxdxbxegedabagacad
 export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
-export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/lib/python2.7/site-packages:/usr/texbin:$PATH"
-export PATH="/opt/homebrew/bin:$PATH"
+# PATH configuration
+# Add Homebrew paths (for both Intel and Apple Silicon Macs)
+[[ -d /opt/homebrew/bin ]] && export PATH="/opt/homebrew/bin:$PATH"
+[[ -d /usr/local/bin ]] && export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
