b5b2e2b59c8beaea2f6a5c1d664d046fa8665de8
winky <y.kohada@gmail.com>
2026-05-08 18:00:58 +0900

    chore: Remove yajs.vim duplicate JavaScript syntax plugin
    yajs.vim has been stale for 5+ years and overlaps with pangloss/vim-javascript
    which is already configured for both Vim and Neovim. Drop yajs.vim from both
    ecosystems. vim-over (Vim) is left in place because Neovim's inccommand
    already covers that need but Vim has no built-in equivalent.
    
    Closes #15

--- config/nvim/lua/plugins/javascript.lua
+++ config/nvim/lua/plugins/javascript.lua
@@ -1,7 +1,6 @@
 -- JavaScript syntax support
 return {
   { "pangloss/vim-javascript", ft = { "javascript", "javascript.jsx" } },
-  { "othree/yajs.vim", ft = "javascript" },
   config = function()
     vim.g.javascript_plugin_jsdoc = 1
   end,
